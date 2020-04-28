3690586833b05fe02c9e8b4e7c3338c200d5d672
JulyChenteng <enumhack@163.com>
2020-04-29 00:06:16 +0800

    update

--- makeexample/ch06-staticlib/lib/division.h
+++
@@ -1,8 +0,0 @@
-//demostrate for make file
-//author: Leo.C.Wu
-//header: division.h
-
-#ifndef __DIVISION
-#define __DIVISION
-double divide(double a, double b);
-#endif
--- makeexample/perl-lib/header/libop.h
+++
@@ -1,4 +0,0 @@
-extern double xadd(double a, double b);
-extern double xdiv(double a, double b);
-extern double xsub(double a, double b);
-extern double xmul(double a, double b);
